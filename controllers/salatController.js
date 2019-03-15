146a8622f65e4e2c67183ffd7a03d17b39bb6e6d
abderrahman-teta <abderrahman.teta95@gmail.com>
2019-03-15 11:40:21 +0100

    add ishaa

--- controllers/salatController.js
+++ controllers/salatController.js
@@ -1,19 +1,24 @@
 const salattime = require('../models/salattime')
 
+const dateFormat = require('dateformat')
+
+
 function addNewDay(req,res) {
 
+    const date =new Date()
+    
     const times = [
         {
             "الفجر":req.body.fajer,
             "الظهر":req.body.doher,
             "العصر":req.body.aser,
             "المغرب":req.body.maghrib,
-            "العشاء":"08:45",
+            "العشاء":req.body.ishaa,
         }
     ]
     const prams = new salattime({
         date_hijri :req.body.date_hijri,
-        date_miladi : new Date(),
+        date_miladi : date,
         times :times
     })
     prams.save()
